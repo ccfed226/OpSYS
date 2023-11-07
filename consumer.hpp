3b355733c9239f2d8997f43d21d34d09eaaeb59e
ccfed226 <142843018+ccfed226@users.noreply.github.com>
2023-11-07 15:31:52 -0500

    Create consumer.hpp

---
+++ consumer.hpp
@@ -0,0 +1,13 @@
+#ifndef CONSUMER_HPP
+#define CONSUMER_HPP
+
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <iostream>
+#include <fcntl.h>
+#include <sys/mman.h>
+
+int main(int, char **);
+
+#endif
