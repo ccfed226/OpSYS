09e167f390aec2295ba72fc38e2460fbf45853a3
ccfed226 <142843018+ccfed226@users.noreply.github.com>
2023-11-07 15:31:07 -0500

    Create producer.hpp

---
+++ producer.hpp
@@ -0,0 +1,13 @@
+#ifndef PRODUCER_HPP
+#define PRODUCER_HPP
+
+    #include <stdio.h>
+    #include <unistd.h>
+    #include <pthread.h>
+    #include <iostream>
+    #include <fcntl.h>
+    #include <sys/mman.h>
+
+    int main(int, char**);
+
+#endif
